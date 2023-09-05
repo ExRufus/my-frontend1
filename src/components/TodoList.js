f1fc0a3373831563ca275818be4473e196380a42
ExRufus <akmaldyn02@gmail.com>
2023-09-05 22:25:44 +0700

    done

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,7 +8,7 @@ const TodoListPage = () => {
 
   const getTodos = async () => {
     try {
-      const response = await fetch("https://gray-quaint-jay.cyclic.app", {
+      const response = await fetch("https://gray-quaint-jay.cyclic.app/todos", {
         method: "GET",
         headers: { token: localStorage.token },
       });
@@ -25,7 +25,7 @@ const TodoListPage = () => {
       setLoading(true);
       const user_id = localStorage.token;
 
-      const response = await fetch("https://gray-quaint-jay.cyclic.app", {
+      const response = await fetch("https://gray-quaint-jay.cyclic.app/todos", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
