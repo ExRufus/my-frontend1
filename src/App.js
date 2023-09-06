fe6702c30bef560a473622552bd9e016bc3cb64d
ExRufus <akmaldyn02@gmail.com>
2023-09-06 21:34:43 +0700

    done

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ toast.configure();
 function App() {
   const checkAuthenticated = async () => {
     try {
-      const res = await fetch("https://fsw-todo-list-app.vercel.app/is-verify", {
+      const res = await fetch("https://gray-quaint-jay.cyclic.app/is-verify", {
         method: "POST",
         headers: { jwt_token: localStorage.token }
       });
